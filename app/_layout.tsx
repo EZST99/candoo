0dffa229c38200ae9c01d697e534e80dada0b197
Noah Howadt <noah.howadt@gmail.com>
2024-03-30 16:13:20 +0100

    Inter font + header + input styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,21 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { Text, View } from "react-native";
 import AuthProvider from "../common/AuthProvider";
 
 function AppLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter: require("../assets/Inter.ttf"),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <AuthProvider>
       <Stack>
--- common/Login.tsx
+++ common/Login.tsx
@@ -1,7 +1,6 @@
 import * as SecureStore from "expo-secure-store";
 import React, { useState } from "react";
-import { Button, Text, TextInput } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import { useUser } from "./AuthProvider";
 
 function Login() {
@@ -29,22 +28,69 @@ function Login() {
   }
 
   return (
-    <SafeAreaView>
-      <Text>Login</Text>
-      <TextInput
-        value={username}
-        onChangeText={setUsername}
-        placeholder="Username"
-      />
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password"
-        secureTextEntry
-      />
-      <Button title="Login" onPress={handleLogin} />
-    </SafeAreaView>
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.headerContent}>Candoo</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Login</Text>
+        <TextInput
+          value={username}
+          onChangeText={setUsername}
+          placeholder="Username"
+          style={styles.input}
+        />
+        <TextInput
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password"
+          secureTextEntry
+          style={styles.input}
+        />
+        <Button title="Login" onPress={handleLogin} />
+      </View>
+    </View>
   );
 }
+
+/*
+
+
+position: absolute;
+width: 308px;
+height: 66px;
+left: 42px;
+top: 297px;
+
+background: rgba(255, 0, 0, 0.23);
+border-radius: 10px;
+
+
+*/
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#ff0000b8",
+    height: 180,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerContent: {
+    fontSize: 60,
+    lineHeight: 73,
+    fontFamily: "Inter",
+    fontWeight: "700",
+    color: "#FFFFFF",
+  },
+  container: {
+    justifyContent: "center",
+    padding: 24,
+  },
+  input: {
+    backgroundColor: "#ff00003b",
+    padding: 10,
+    borderRadius: 10,
+  },
+});
 
 export default Login;
