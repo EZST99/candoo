c6711fde648e0896fb614a313c1c35b7f7a43b85
Irem <irem.arslan@gmx.at>
2024-04-01 17:02:15 +0200

    fixed db connection issues

--- common/db/connection.js
+++ common/db/connection.js
@@ -1,29 +1,13 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-// Verbindungsdaten
-const connectionData = {
-    host: "host",
-    user: "user",
-    password: "password",
-    database: "database",
-    servername: "localhost",
-};
-
-// Zuerst eine Verbindung zum MySQL-Server ohne Datenbankangabe
-const initialConnection = await mysql.createConnection({
-    host: connectionData.host,
-    user: connectionData.user,
-    password: connectionData.password,
-    servername: connectionData.servername,
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || " ",
+  database: process.env.DB_NAME || "db_candoo",
 });
 
-// Erstellen der Datenbank, falls sie nicht existiert
-await initialConnection.query(`CREATE DATABASE IF NOT EXISTS \`${connectionData.database}\``);
-await initialConnection.end(); // Schließen der initialen Verbindung
-
-// Jetzt eine Verbindung zur spezifischen Datenbank
-const connection = await mysql.createConnection(connectionData);
-
-// Initialisiere Drizzle mit der Verbindung
 const db = drizzle(connection);
+
+export default db;
