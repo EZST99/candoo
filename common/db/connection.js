955522fd15202bb9710c09ae72b6be375529d0ff
Irem <irem.arslan@gmx.at>
2024-04-01 17:33:23 +0200

    fixed connection issue

--- common/db/connection.js
+++ common/db/connection.js
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise";
 const connection = await mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || " ",
+  password: "",
   database: process.env.DB_NAME || "db_candoo",
 });
 
