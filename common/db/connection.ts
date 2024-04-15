a09d6d713f8a39c12b2267cc0ba13d2c38c96f90
Noah Howadt <noah.howadt@gmail.com>
2024-04-15 16:49:50 +0200

    Add env variable checks

--- common/db/connection.ts
+++ common/db/connection.ts
@@ -1,11 +1,17 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2";
 
+if (!process.env.DB_HOST) throw new Error("DB_HOST not found");
+if (!process.env.DB_USER) throw new Error("DB_USER not found");
+if (!process.env.DB_NAME) throw new Error("DB_NAME not found");
+if (!process.env.DB_PASS)
+  console.log("DB_PASS not found, using empty password");
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
-  password: "",
+  password: process.env.DB_PASS || "",
 });
 
 const db = drizzle(connection);
--- common/db/drizzle.config.ts
+++ common/db/drizzle.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "drizzle-kit";
-console.log("DB_HOST", process.env.DB_HOST);
+
+if (!process.env.DB_HOST) throw new Error("DB_HOST not found");
+if (!process.env.DB_USER) throw new Error("DB_USER not found");
+if (!process.env.DB_NAME) throw new Error("DB_NAME not found");
+if (!process.env.PASS) console.log("DB_PASS not found, using empty password");
+
 export default defineConfig({
   schema: "./common/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
     password: process.env.PASS || "",
-    database: process.env.DB_NAME || "db_candoo",
   },
   verbose: true,
   strict: true,
