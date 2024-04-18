799d869af37e742e54ab7dc12f9e44afecad1b0b
Irem <irem.arslan@gmx.at>
2024-04-18 11:34:25 +0200

    viewtasks angelegt

---
+++ app/api/taskView+api.ts
@@ -0,0 +1,11 @@
+import db from "../../common/db/connection";
+import { tasks } from "../../common/db/schema";
+
+export async function GET(request: Request) {
+    const result = await db.select().from(tasks);
+    return new Response(JSON.stringify(result), {
+    });
+
+    
+    }
+
