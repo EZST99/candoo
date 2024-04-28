26a215e629735b00c5fb89206dd61499d63374bb
Noah Howadt <noah.howadt@gmail.com>
2024-04-28 13:45:45 +0200

    Tabs style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,42 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarActiveBackgroundColor: "#ff0000b8",
+        tabBarInactiveBackgroundColor: "#ff0000b8",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="taskscreate"
         options={{
           title: "Create Task",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="plus" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
