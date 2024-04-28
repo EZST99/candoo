13bd1ef09840fb0741b873db5cf515e42edcbd9f
Dicle <dicle.bbr@gmail.com>
2024-04-28 22:09:16 +0200

    frontend für die einzelnen tasks

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -1,39 +1,146 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native'; // Import the View component
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import CheckBox from "expo-checkbox";
+import { tasks } from "../../common/db/schema";
+import { check } from "drizzle-orm/pg-core";
+import TouchableOpacity from "../../common/components/TouchableOpacity";
+import { AntDesign } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import ButtonCircle from "../../common/components/PlusButton";
+import { router, useRouter } from "expo-router";
 
 interface Task {
-    task_id: number;
-    taskname: string;
+  task_id: number;
+  taskname: string;
 }
 
 export default function Tasks() {
-    const [tasks, setTasks] = useState<Task[]>([]); // Define the type for tasks array
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [selectedTasks, setSelectedTasks] = useState<number[]>([]); // Zustand für die ausgewählten Tasks
 
-    useEffect(() => {
-        getTasks();
-    }, []);
+  useEffect(() => {
+    getTasks();
+  }, []);
 
-    async function getTasks() {
-        try {
-            const response = await fetch("/api/taskView", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            const data = await response.json();
-            setTasks(data); // Hier werden die empfangenen Daten in den State gesetzt
-        } catch (error) {
-            console.error(error);
-        }
+  async function getTasks() {
+    try {
+      const response = await fetch("/api/taskView", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    return (
-        <View>
-            {/* Hier renderst du die gefetchten Elemente */}
-            {tasks.map(task => (
-                    <Text>{task.taskname}</Text>
-            ))}
-        </View>
+  // Funktion zum Aktualisieren des ausgewählten Zustands eines Tasks
+  const handleTaskSelection = (taskId: number) => {
+    setSelectedTasks((prevSelectedTasks) =>
+      prevSelectedTasks.includes(taskId)
+        ? prevSelectedTasks.filter((id) => id !== taskId)
+        : [...prevSelectedTasks, taskId]
     );
+  };
+
+  return (
+    <>
+    <View style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["rgba(255, 0, 0, 0.72)", "white"]}
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          top: 0,
+          height: "110%",
+        }}
+      />
+      {/* Today's Tasks */}
+      <Text style={styles.sectionTitle}>Tasks</Text>
+
+      {/* Rendern der gefetchten Elemente */}
+      {tasks.map((task) => (
+        <View key={task.task_id} style={styles.item}>
+          <View style={styles.itemLeft}>
+            <View style={styles.square}></View>
+            <Text>{task.taskname}</Text>
+          </View>
+          <CheckBox
+            value={selectedTasks.includes(task.task_id)}
+            onValueChange={() => handleTaskSelection(task.task_id)}
+          />
+        </View>
+      ))}
+      {/* "+"-Button am unteren Rand */}
+    </View>
+      <View style={styles.addButton}>  
+      <ButtonCircle onPress={() => router.push('taskscreate')}>
+        <View>
+      <AntDesign name="plus" size={24} color="white" />
+        </View>
+      </ButtonCircle>
+      </View>
+      </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    /*alignItems: "center",
+    justifyContent: "center",*/
+  },
+  tasksWrapper: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  items: {
+    marginTop: 30,
+  },
+  item: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  addButton: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ common/components/PlusButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, TouchableOpacityProps, TouchableOpacity as TouchableReact } from "react-native";
+
+function ButtonCircle(props: TouchableOpacityProps) {
+  return <TouchableReact {...props} style={styles.base} />;
+}
+
+const styles = StyleSheet.create({
+  base: {
+    backgroundColor: "rgba(255, 0, 0, 0.72)",
+    padding: 10,
+    height: 60,
+    width: 60,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default ButtonCircle;
