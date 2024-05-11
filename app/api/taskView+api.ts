edcf22a6a80791e06d9bf081c1133f8714368d6d
Kenn-Michael Sanga <kennsanga@gmail.com>
2024-05-11 16:01:07 +0200

    api change

--- app/api/taskView+api.ts
+++ app/api/taskView+api.ts
@@ -5,29 +5,9 @@ import { tasks } from "../../common/db/schema";
 
 export async function GET(request: Request) {
   const user = await authenticateUser(request);
-  let category_id;
-  if (request.url.includes("?")) {
-    category_id = request.url.split("?")[1].split("=")[1];
-  }
-
-  // query all tasks if no category_id exists
-  if (category_id === undefined) {
     const result = await db
       .select()
       .from(tasks)
       .where(eq(tasks.user_id, user.user_id));
     return new Response(JSON.stringify(result), {});
-  }
-
-  // query tasks by category_id
-  const result = await db
-    .select()
-    .from(tasks)
-    .where(
-      and(
-        eq(tasks.category_id, parseInt(category_id)),
-        eq(tasks.user_id, user.user_id)
-      )
-    );
-  return new Response(JSON.stringify(result), {});
 }
