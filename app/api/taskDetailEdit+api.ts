b4f377b70196ac8b8fdc42e1015d78f80789f970
Irem <irem.arslan@gmx.at>
2024-05-11 17:30:35 +0200

    taskDetailEdit backend is done

---
+++ app/api/taskDetailEdit+api.ts
@@ -0,0 +1,39 @@
+import { and, eq } from "drizzle-orm";
+import authenticateUser from "../../common/db/authenticateUser";
+import db from "../../common/db/connection";
+import { tasks } from "../../common/db/schema";
+
+export interface TaskUpdateRequest {
+  task_id: number;
+  category_id: number;
+  taskname: string;
+  due_date: Date;
+  description: string;
+  importance: number;
+  urgency: number;
+}
+
+export async function PUT(request: Request) {
+  const body = (await request.json()) as TaskUpdateRequest;
+  const user = await authenticateUser(request);
+
+  // Überprüfen, ob der Benutzer die Berechtigung hat, diese Aufgabe zu bearbeiten
+
+  await db
+    .update(tasks)
+    .set({
+      category_id: body.category_id,
+      taskname: body.taskname,
+      due_date: body.due_date,
+      description: body.description,
+      importance: body.importance,
+      urgency: body.urgency,
+    })
+    .where(
+      and(eq(tasks.task_id, body.task_id), eq(tasks.user_id, user.user_id))
+    );
+
+  return new Response(JSON.stringify({ success: true }), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
