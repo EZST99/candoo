a5133f812ac18265aabe948d5951d69e78990fc9
Noah Howadt <noah.howadt@gmail.com>
2024-05-23 20:49:22 +0200

    sorting

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -1,23 +1,22 @@
-import { AntDesign } from "@expo/vector-icons";
-import { InferSelectModel } from "drizzle-orm";
-import CheckBox from "expo-checkbox";
-import { LinearGradient } from "expo-linear-gradient";
-import { useFocusEffect, useLocalSearchParams, useRouter } from "expo-router";
-import React, { useCallback, useState } from "react";
+import { AntDesign, FontAwesome, FontAwesome6 } from '@expo/vector-icons';
+import { InferSelectModel } from 'drizzle-orm';
+import CheckBox from 'expo-checkbox';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useFocusEffect, useLocalSearchParams, useRouter } from 'expo-router';
+import React, { useCallback, useState } from 'react';
 import {
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
-  SafeAreaView,
-  TextInput,
-} from "react-native";
-import authenticatedFetch from "../../common/authenticatedFetch";
-import ButtonCircle from "../../common/components/PlusButton";
-import { tasks as tasksTable } from "../../common/db/schema";
-import { MarkAsDoneResult } from "../api/markAsDone+api";
-import { FontAwesome6 } from "@expo/vector-icons";
+} from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import authenticatedFetch from '../../common/authenticatedFetch';
+import ButtonCircle from '../../common/components/PlusButton';
+import { tasks as tasksTable } from '../../common/db/schema';
+import { MarkAsDoneResult } from '../api/markAsDone+api';
 //import { mdiMagnify } from '@mdi/js';
 
 interface Category {
@@ -33,7 +32,10 @@ export default function Tasks() {
   const router = useRouter();
   const { category_id } = useLocalSearchParams<{ category_id: string }>();
   const [category, setCategory] = useState<Category[] | undefined>([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sort, setSort] = useState<'smart' | 'importance' | 'due_date'>(
+    'smart'
+  );
 
   useFocusEffect(
     useCallback(() => {
@@ -44,7 +46,7 @@ export default function Tasks() {
 
   async function getTasks() {
     try {
-      let url = "/api/taskView";
+      let url = '/api/taskView';
       if (category_id !== undefined) {
         url = `/api/taskView?category_id=${category_id}`;
       }
@@ -52,9 +54,9 @@ export default function Tasks() {
       let data = await authenticatedFetch<
         Array<InferSelectModel<typeof tasksTable>>
       >(url, {
-        method: "GET",
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
 
@@ -81,9 +83,9 @@ export default function Tasks() {
 
       let url = `/api/categoryDetails?category_id=${category_id}`;
       const data = await authenticatedFetch(url, {
-        method: "GET",
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
 
@@ -97,9 +99,9 @@ export default function Tasks() {
     try {
       const url = `/api/categoryDetails?category_id=${category_id}`;
       const data = await authenticatedFetch(url, {
-        method: "GET",
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
       return data[0]?.color;
@@ -117,10 +119,10 @@ export default function Tasks() {
       )
     );
 
-    authenticatedFetch<MarkAsDoneResult>("/api/markAsDone", {
-      method: "POST",
+    authenticatedFetch<MarkAsDoneResult>('/api/markAsDone', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         task_id: taskId,
@@ -135,7 +137,7 @@ export default function Tasks() {
         );
       })
       .catch((error) => {
-        console.error("Task selection failed:", error);
+        console.error('Task selection failed:', error);
       });
   };
 
@@ -182,47 +184,136 @@ export default function Tasks() {
   // Anwenden des Filters auf das tasks-Array
   const filteredTasks = tasks.filter(filterTasks);
 
+  function sortSmart(
+    a: InferSelectModel<typeof tasksTable>,
+    b: InferSelectModel<typeof tasksTable>
+  ) {
+    const dueDateA = new Date(a.due_date);
+    const dueDateB = new Date(b.due_date);
+
+    // if a is more important and more urgent than b, rank a higher
+    if (
+      a.importance > b.importance &&
+      dueDateA.getTime() < dueDateB.getTime()
+    ) {
+      return -1;
+    }
+
+    // if b is more important and more urgent than a, rank b higher
+    if (
+      b.importance > a.importance &&
+      dueDateB.getTime() < dueDateA.getTime()
+    ) {
+      return 1;
+    }
+
+    // if a is more important than b, rank a higher
+    if (a.importance > b.importance) {
+      return -1;
+    }
+
+    // if b is more important than a, rank b higher
+    if (b.importance > a.importance) {
+      return 1;
+    }
+
+    // if a is more urgent than b, rank a higher
+    if (dueDateA.getTime() < dueDateB.getTime()) {
+      return -1;
+    }
+
+    // if b is more urgent than a, rank b higher
+    if (dueDateB.getTime() < dueDateA.getTime()) {
+      return 1;
+    }
+
+    return 0;
+  }
   return (
     <>
-      <SafeAreaView style={{ backgroundColor: "#FF0000" }}>
-        <TextInput
-          style={styles.search}
-          placeholder="Search"
-          onChangeText={handleSearchTermChange}
-        />
-        <FontAwesome6 name="magnifying-glass" style={styles.magnifyer} />
-      </SafeAreaView>
+      <View style={{ backgroundColor: '#FF0000' }}>
+        <View>
+          <TextInput
+            style={styles.search}
+            placeholder='Search'
+            onChangeText={handleSearchTermChange}
+          />
+          <FontAwesome6 name='magnifying-glass' style={styles.magnifyer} />
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <FontAwesome
+            name='sort-amount-desc'
+            size={24}
+            color='white'
+            style={{ marginHorizontal: 15, marginVertical: 5, padding: 5 }}
+          />
+          <Dropdown
+            style={styles.dropdown}
+            data={
+              [
+                { label: 'Smart', value: 'smart' },
+                { label: 'Due Date', value: 'due_date' },
+                { label: 'Importance', value: 'importance' },
+              ] as const
+            }
+            placeholderStyle={{ color: 'rgba(0, 0, 0, 0.19)' }}
+            labelField='label'
+            valueField='value'
+            value={sort}
+            // placeholder={!isFocus ? "Select category" : "..."}
+            onChange={(item) => {
+              setSort(item.value);
+            }}
+          />
+        </View>
+      </View>
 
       <View style={styles.container}>
         <LinearGradient
-          colors={["rgba(255, 0, 0, 0.72)", "white"]}
+          colors={['rgba(255, 0, 0, 0.72)', 'white']}
           style={{
-            position: "absolute",
+            position: 'absolute',
             left: 0,
             right: 0,
             top: 0,
-            height: "110%",
+            height: '110%',
           }}
         />
         <Text style={styles.sectionTitle}>
-          {category_id ? category?.[0]?.categoryname + " \n" : "All "}Tasks
+          {category_id ? category?.[0]?.categoryname + ' \n' : 'All '}Tasks
         </Text>
         <ScrollView>
-          {filteredTasks.map((task) => (
-            <TaskItem
-              taskname={task.taskname}
-              color={task.color}
-              task_id={task.task_id.toString()}
-              is_done={task.is_done}
-              key={task.task_id}
-            />
-          ))}
+          {filteredTasks
+            .sort((a, b) => {
+              switch (sort) {
+                case 'smart':
+                  return sortSmart(a, b);
+                case 'due_date':
+                  return (
+                    new Date(a.due_date).getTime() -
+                    new Date(b.due_date).getTime()
+                  );
+                case 'importance':
+                  return b.importance - a.importance;
+                default:
+                  return 0;
+              }
+            })
+            .map((task) => (
+              <TaskItem
+                taskname={task.taskname}
+                color={task.color}
+                task_id={task.task_id.toString()}
+                is_done={task.is_done}
+                key={task.task_id}
+              />
+            ))}
         </ScrollView>
       </View>
       <View style={styles.addButton}>
-        <ButtonCircle onPress={() => router.push("taskscreate")}>
+        <ButtonCircle onPress={() => router.push('taskscreate')}>
           <View>
-            <AntDesign name="plus" size={24} color="white" />
+            <AntDesign name='plus' size={24} color='white' />
           </View>
         </ButtonCircle>
       </View>
@@ -244,29 +335,29 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
 
-    fontFamily: "Inter",
-    fontWeight: "700",
-    color: "#fff",
-    alignSelf: "center",
-    textAlign: "center",
+    fontFamily: 'Inter',
+    fontWeight: '700',
+    color: '#fff',
+    alignSelf: 'center',
+    textAlign: 'center',
     marginBottom: 20,
   },
   items: {
     marginTop: 30,
   },
   item: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     padding: 20,
     borderRadius: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     marginBottom: 10,
   },
   itemLeft: {
-    flexDirection: "row",
-    alignItems: "center",
-    flexWrap: "wrap",
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
   },
   square: {
     width: 24,
@@ -275,31 +366,41 @@ const styles = StyleSheet.create({
     marginRight: 15,
   },
   itemText: {
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   addButton: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 24,
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   search: {
     height: 40,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 10,
     margin: 10,
-    paddingLeft: 30, 
-    position: "relative", 
+    paddingLeft: 30,
+    position: 'relative',
   },
   magnifyer: {
-    position: "absolute",
-    top: 20, 
-    left: 15, 
+    position: 'absolute',
+    top: 20,
+    left: 15,
     fontSize: 20,
-    color: "gray",
+    color: 'gray',
     opacity: 0.5,
+  },
+  dropdown: {
+    backgroundColor: '#fff',
+    borderWidth: 3,
+    borderColor: 'rgba(0, 0, 0, 0.19)',
+    paddingHorizontal: 10,
+    marginVertical: 5,
+    borderRadius: 10,
+    textAlign: 'center',
+    width: 200,
   },
 });
