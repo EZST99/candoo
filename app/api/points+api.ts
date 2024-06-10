a588e2fb07c29e22d368f30e785a4e2967fb2cfc
Noah Howadt <noah.howadt@gmail.com>
2024-06-10 22:43:44 +0200

    fix points aggregation

--- app/api/points+api.ts
+++ app/api/points+api.ts
@@ -7,13 +7,15 @@ export async function GET(request: Request) {
   const user = await authenticateUser(request);
   const res = await db
     .select({
-      points: sql`${tasks.importance} + DATEDIFF(${tasks.due_date}, ${tasks.completed_at})`,
+      points: sql`SUM(${tasks.importance} + DATEDIFF(${tasks.due_date}, ${tasks.completed_at}))`,
     })
     .from(tasks)
     .where(and(eq(tasks.user_id, user.user_id), eq(tasks.is_done, true)))
     .groupBy(tasks.user_id);
 
-  return new Response(JSON.stringify({ points: res[0]?.points || 0 }), {
+  console.log(res);
+
+  return new Response(JSON.stringify({ points: Number(res[0]?.points) || 0 }), {
     headers: { 'Content-Type': 'application/json' },
   });
 }
